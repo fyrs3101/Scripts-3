536b3edf6460de06108cb87a9ce2b7a295fe7fb8
ouyangzongheng <ouyangzoheng@163.com>
2020-12-18 10:18:14 +0800

    互助码

---
+++ crontab_list.sh
@@ -0,0 +1,14 @@
+0 3 1,10,20 * *  /usr/bin/curl http://api.turinglabs.net/api/v1/jd/bean/create/qlnvpggfan6gr56h4a764rocbe >> /home/log/jd.log 2>&1
+0 3 1,10,20 * *  /usr/bin/curl http://api.turinglabs.net/api/v1/jd/farm/create/f516b303c2f24589b278552398c2cd59
+0 3 1,10,20 * *  /usr/bin/curl http://api.turinglabs.net/api/v1/jd/pet/create/MTE1NDUyMjEwMDAwMDAwMzQxMDE3MjE=
+0 3 1,10,20 * *  /usr/bin/curl http://api.turinglabs.net/api/v1/jd/ddfactory/create/P04z54XCjVWnYaS5mpQWjWx
+0 3 1,10,20 * *  /usr/bin/curl http://api.turinglabs.net/api/v1/jd/jxfactory/create/Vg_-e5pPUaU3YhvHVn0P9Q==
+0 3 1,10,20 * *  /usr/bin/curl http://api.turinglabs.net/api/v1/jd/bean/create/zqgoemkzqn36uf5voba77vgjorzqyv3dqcwudoha7kw73ou4wvza
+0 3 1,10,20 * *  /usr/bin/curl http://api.turinglabs.net/api/v1/jd/farm/create/cbcb32ca40f8424c8bccb941d28473bd
+0 3 1,10,20 * *  /usr/bin/curl http://api.turinglabs.net/api/v1/jd/pet/create/MTE1NDAxNzYwMDAwMDAwMzQxMDE5NjU=
+0 3 1,10,20 * *  /usr/bin/curl http://api.turinglabs.net/api/v1/jd/ddfactory/create/P04z54XCjVWnYaS5uOwqbZLXqnuFPJ6LxVFG-TasvLIIUrtNVM
+0 3 1,10,20 * *  /usr/bin/curl http://api.turinglabs.net/api/v1/jd/jxfactory/create/465yQSB5CxikqSPDxXh0UkP_KrMTrBQxM2V00Xo3L8c=
+
+
+
+
